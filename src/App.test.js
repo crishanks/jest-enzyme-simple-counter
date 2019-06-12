3e1018cf5e0ff70bd056b167eac2879bed8f2b34
github username <crishanks12@gmail.com>
2019-06-11 18:34:14 -0600

    basic test setup

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Testing Frameworks
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter()});
+
+test('renders without error', () => {
+
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
 });
