75fd86971e174c077b243ccb4c4d098dfa85c03a
github username <crishanks12@gmail.com>
2019-06-12 18:36:19 -0600

    added button click functionality to App component, clicking button increments counter display testing added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
   render() {
     return (
       <div data-test="component-app">
-        <h1 data-test="counter-display">The count is currently</h1>
-        <button data-test="increment-button">Increment counter</button>
+        <h1 data-test="counter-display">The count is currently {this.state.counter} </h1>
+        <button 
+          data-test="increment-button"
+          onClick={() => this.setState({counter: this.state.counter + 1})}
+        >Increment counter</button>
       </div>
     );
   }
